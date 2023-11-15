e6a062492f36f4bfe73eaf80a699a6858432019d
wajaat-shah-mac-in-cloud <wajaatshah101@gmail.com>
2023-11-15 07:16:05 -0600

    banner styling update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,12 +16,11 @@ export class HomeComponent implements OnInit {
   // Todo - setup api call to get the actually adhan and iqamah times and remove mock data
   private getPrayerTimes() {
     this.prayerTimesList = {
-      fajr : { adhan: '5:00 am', iqamah: '5:15 am' },
-      dhur : { adhan: '1:00 pm', iqamah: '1:20 pm' },
-      asr : { adhan: '3:00 pm', iqamah: '3:15 pm' },
-      maghrib : { adhan: '5:30 pm', iqamah: '5:45 pm' },
-      isha : { adhan: '7:20 pm', iqamah: '7:30 pm' },
+      fajr : { adhan: '5:45 am', iqamah: '6:20 am' },
+      dhur : { adhan: '12:12 pm', iqamah: '1:00 pm' },
+      asr : { adhan: '3:00 pm', iqamah: '3:30 pm' },
+      maghrib : { adhan: '5:28 pm', iqamah: '5:35 pm' },
+      isha : { adhan: '7:15 pm', iqamah: '7:30 pm' },
    }
   }
-
 }
